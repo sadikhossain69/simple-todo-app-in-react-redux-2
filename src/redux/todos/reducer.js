c60aedb3b05d09cca78eb205eca264763bfb5495
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-23 14:49:35 +0600

    defining reducer for todo slice

---
+++ src/redux/todos/reducer.js
@@ -0,0 +1,59 @@
+import { ADDED, ALLCOMPLETED, CLEARCOMPLETED, COLORSELECTED, DELETED, TOGGLED } from "./actionTypes";
+import { initialState } from "./initialState";
+
+const nextTodoId = (todos) => {
+    const maxId = todos.reudce((maxId, todo) => Math.max(todo.id, maxId), -1)
+    return maxId + 1
+}
+
+export const reudcer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADDED:
+            return [
+                ...state,
+                {
+                    id: nextTodoId(state)
+                }
+            ]
+
+        case ALLCOMPLETED:
+            return state.map(todo => {
+                return {
+                    ...todo,
+                    completed: true,
+                }
+            })
+
+        case CLEARCOMPLETED:
+            return state.filter(todo => !todo.completed)
+
+        case COLORSELECTED:
+            const { todoId, color } = action.payload
+            return state.map(todo => {
+                if (todo.id !== todoId) {
+                    return todo
+                }
+                return {
+                    ...todo,
+                    color: color
+                }
+            })
+
+        case DELETED:
+            return state.filter(todo => todo.id !== action.payload);
+
+        case TOGGLED:
+            return state.map(todo => {
+                if (todo.id !== action.payload) {
+                    return todo
+                }
+                return {
+                    ...todo,
+                    completed: !todo.completed,
+                }
+            })
+
+        default:
+            return state
+    }
+}
